233188347b388950d668f398c61cdd997fe214c6
radwan20203 <bovag53453@hupoi.com>
2025-06-15 22:59:25 +0300

    refactor Map markers: using local map data

--- lib/map/widgets/custom_google_map.dart
+++ lib/map/widgets/custom_google_map.dart
@@ -1,30 +1,31 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+//import 'package:google_maps_app/models/place_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 
-//import 'change_location_button.dart';
+import '../models/place_model.dart';
+import 'change_location_button.dart';
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
+
   @override
   State<CustomGoogleMap> createState() => _CustomGoogleMapState();
 }
 
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
-  GoogleMapController? googleMapController;
-  Set<Marker> markers = {};
+  GoogleMapController? googleMapController; // Nullable controller
+  String? nightMapStyle;
 
   @override
   void initState() {
-    super.initState();
     initialCameraPosition = const CameraPosition(
-      target: LatLng(26.8206, 30.8025), // مركز مصر
-      zoom: 6,
+      target: LatLng(31.187084851056554, 29.928110526889437),
+      zoom: 12,
     );
-    fetchAndShowHospitals();
+    //initMapStyle();
+    initMarkers();
+    super.initState();
   }
 
   @override
@@ -33,44 +34,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     super.dispose();
   }
 
-  Future<void> fetchAndShowHospitals() async {
-    try {
-      final places = await fetchHospitals();
-      final icon =
-          BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed);
-
-      final newMarkers = places.map((p) {
-        final loc = p['geometry']['location'];
-        return Marker(
-          markerId: MarkerId(p['place_id']),
-          position: LatLng(loc['lat'], loc['lng']),
-          infoWindow: InfoWindow(title: p['name']),
-          icon: icon,
-        );
-      }).toSet();
-
-      setState(() {
-        markers = newMarkers;
-      });
-    } catch (e) {
-      debugPrint('Error fetching hospitals: $e');
-    }
-  }
-
-  Future<List<Map<String, dynamic>>> fetchHospitals() async {
-    final apiKey = dotenv.env['ANDROID_MAP_API_KEY'];
-    final url =
-        Uri.parse('https://maps.googleapis.com/maps/api/place/textsearch/json'
-            '?query=hospital+in+Egypt&key=$apiKey');
-
-    final resp = await http.get(url);
-    final data = json.decode(resp.body);
-    if (data['status'] == 'OK') {
-      return List<Map<String, dynamic>>.from(data['results']);
-    } else {
-      throw Exception('Places API error: ${data['status']}');
-    }
-  }
+  Set<Marker> markers = {};
 
   @override
   Widget build(BuildContext context) {
@@ -78,18 +42,44 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       body: Stack(
         children: [
           GoogleMap(
-            initialCameraPosition: initialCameraPosition,
             markers: markers,
-            onMapCreated: (c) {
-              googleMapController = c;
+            //style: nightMapStyle,
+            onMapCreated: (contrller) {
+              googleMapController = contrller;
               setState(() {});
             },
-            myLocationEnabled: true,
+            initialCameraPosition: initialCameraPosition,
           ),
-          // if (googleMapController != null)
-          //   ChangeLocationButton(googleMapController: googleMapController!),
+          if (googleMapController != null)
+            ChangeLocationButton(googleMapController: googleMapController!),
         ],
       ),
     );
+  }
+
+  // void initMapStyle() async {
+  //   nightMapStyle = await DefaultAssetBundle.of(context)
+  //       .loadString('assets/map_styles/night_map_style.json');
+  //   setState(() {});
+  // }
+
+  void initMarkers() async {
+    var customMarkerIcon = await BitmapDescriptor.asset(
+        const ImageConfiguration(), 'assets/images/icons8-marker-50.png');
+    var myMarkers = places
+        .map(
+          (placeModel) => Marker(
+            markerId: MarkerId(placeModel.id.toString()),
+            position: placeModel.latLng,
+            infoWindow: InfoWindow(title: placeModel.name),
+            icon: customMarkerIcon,
+            // change the default marker icon color.
+            //icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          ),
+        )
+        .toSet();
+
+    markers.addAll(myMarkers);
+    setState(() {});
   }
 }
